baac044a33363797235d24594c63248af571289e
Damien Duboeuf <smeagolworms4@gmail.com>
2020-02-28 11:20:48 +0100

    Update UUIDTest.php

--- tests/UUIDTest.php
+++ tests/UUIDTest.php
@@ -1,33 +1,37 @@
 <?php
+
 namespace Test\GollumSF\UUID;
 
 use GollumSF\UUID\UUID;
 use PHPUnit\Framework\TestCase;
 
-class UUIDTest extends TestCase {
+class UUIDTest extends TestCase
+{
+    public function testMinimalUsage()
+    {
+        $uuid = new UUID();
+        $this->assertTrue(!!preg_match('/^[a-f0-9]{8}\-[a-f0-9]{4}\-[a-f0-9]{4}\\-[a-f0-9]{4}\-[a-f0-9]{10}$/', $uuid));
+        $uuid = new UUID('12c634e0-0a86-cec3-03e6-7086f84d98');
+        $this->assertEquals('12c634e0-0a86-cec3-03e6-7086f84d98', $uuid);
+    }
 
-	public function testMinimalUsage() {
-		$uuid = new UUID();
-		$this->assertTrue(!!preg_match('/^[a-f0-9]{8}\-[a-f0-9]{4}\-[a-f0-9]{4}\\-[a-f0-9]{4}\-[a-f0-9]{10}$/', $uuid));
-		$uuid = new UUID('12c634e0-0a86-cec3-03e6-7086f84d98');
-		$this->assertEquals('12c634e0-0a86-cec3-03e6-7086f84d98', $uuid);
-	}
+    public function testException()
+    {
+        $this->expectException(\LogicException::class);
+        new UUID('UNVALID');
+    }
 
-	public function testException() {
-		$this->expectException(\LogicException::class);
-		new UUID('UNVALID');
-	}
+    public function testUniq()
+    {
+        $uuids = [];
+        for ($i = 0; $i < 100000; $i++) {
+            $uuids[] = (new UUID()) . '';
+        }
 
-	public function testUniq() {
-		$uuids = [];
-		for ($i = 0; $i < 100000; $i++) {
-			$uuids[] = (new UUID()).'';
-		}
+        $before = count($uuids);
+        $uuids = array_unique($uuids);
+        $after = count($uuids);
+        $this->assertEquals($before, $after);
+    }
 
-		$before= count($uuids);
-		$uuids = array_unique($uuids);
-		$after= count($uuids);
-		$this->assertEquals($before, $after);
-	}
-	
 }
